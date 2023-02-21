8c90979fee3fc6f2affb5ba13ece4c86f79652e1
deniswadingaf@gmail.com <deniswadinga@gmail.com>
2023-02-21 17:10:55 +0000

    my second commit

--- deploy.py
+++ deploy.py
@@ -2,4 +2,13 @@
 echo hello world
 #shell script
 python (hello world)
-#=python script
+#=python scrip
+
+
+git config --global user.name "deniswadinga"
+
+git config --global user.email deniswadingaf@gmail.com
+
+#to check the configuration,
+
+git config --global --list`wq
